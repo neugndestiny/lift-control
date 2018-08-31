97709998fa8924f7b02b957e10dad0f5d3fdf858
neugndestiny <neungdestiny12@gmail.com>
2018-08-31 15:57:30 +0700

    moveFromTopToG testcase

--- src/test/java/warakorn/springframework/liftcontrol/liftcontrolTest.java
+++ src/test/java/warakorn/springframework/liftcontrol/liftcontrolTest.java
@@ -62,4 +62,11 @@ public class liftcontrolTest {
         String actualResult = liftcontrol.moveDirection("G+1","G");
         assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    public void moveFromTopToG() {
+        String expectedResult = "down";
+        String actualResult = liftcontrol.moveDirection("TOP","G");
+        assertEquals(expectedResult, actualResult);
+    }
 }
